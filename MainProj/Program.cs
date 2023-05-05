d746e53fc77bc894d70dff0789d57338b6a14a1e
Sergey Letov <letovs87@gmail.com>
2023-05-05 22:00:33 +0300

    Реализована логика обработки предполагаемых действий

--- MainProj/Program.cs
+++ MainProj/Program.cs
@@ -14,6 +14,37 @@
              * 5 - Выборка по фильтру (по дефолту) выходной параметр время исполнения
              * 6 - Оптимизация БД для ускорения выполнения работы.
              */
+            if (args.Length > 0)
+            {
+                var action = args[0];
+                switch (action)
+                {
+                    case "1":
+                        Console.WriteLine($"Обработка события {action}");
+                        break;
+                    case "2":
+                        Console.WriteLine($"Обработка события {action}");
+                        break;
+                    case "3":
+                        Console.WriteLine($"Обработка события {action}");
+                        break;
+                    case "4":
+                        Console.WriteLine($"Обработка события {action}");
+                        break;
+                    case "5":
+                        Console.WriteLine($"Обработка события {action}");
+                        break;
+                    case "6":
+                        Console.WriteLine($"Обработка события {action}");
+                        break;
+                    default:
+                        Console.WriteLine("Вы выбрали неизвестное действие.");
+                        break;
+                }
+            }
+            else
+                Console.WriteLine("No action set");
+
         }
     }
 }
