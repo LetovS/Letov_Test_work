6200013c5d44a5d42abadb55508dc23bb5b53b9a
Sergey Letov <letovs87@gmail.com>
2023-05-06 15:17:30 +0300

    Реализована логика создания\удаления базы данных с использованием файла настроек

--- MainProj/Data/MainDBContext.cs
+++ MainProj/Data/MainDBContext.cs
@@ -1,10 +1,61 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MainProj.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace MainProj.Data
 {
     internal class MainDBContext : DbContext
     {
+        private bool IsCreated { get; set; } = false;
+        public DbSet<Person> Persons { get; set; }
+        public MainDBContext()
+        {
+            IsCreated = CheckIsCreatedDB();
+            if (!IsCreated)
+            {
+                Database.EnsureCreated();
+                //TODO записать в файл настроек тру
+                ChangeCreatedDBStatus(isCreated:true);
+            }
+        }
+        /// <summary>
+        /// Проверка создания БД.
+        /// </summary>
+        /// <returns>bool</returns>
+        private bool CheckIsCreatedDB()
+        {
+            if (File.Exists("settings.txt"))
+            {
+                using var sr = new StreamReader("settings.txt");
+                if (sr.ReadToEnd() == "true") return true;
+            }
+            return false;
+        }
 
+        private void ChangeCreatedDBStatus(bool isCreated)
+        {
+            if (isCreated)
+            {                
+                string path = "settings.txt";
+                using var sw = new StreamWriter(path);
+                sw.Write("true");
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LetovTestDB;");
+        }
+
+        internal void EnsureDeleteDB()
+        {
+            //удалить бд
+            Database.EnsureDeleted();
+            // изменить флаг
+            IsCreated = false;
+            // записать false в файл
+            using var sw = new StreamWriter("settings.txt");
+            sw.Write("false");
+        }
     }
 }
